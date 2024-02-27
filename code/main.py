c03a1f590808f672a9d73fb38b6170217b35c95c
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-02-27 11:20:59 -0600

    Update main.py

--- code/main.py
+++ code/main.py
@@ -16,13 +16,19 @@ Purpose:
 
 For more information on the Native Python Application and the Trilobite Coder Lab project, please refer to the project documentation and website.
 """
-from code.app import function1, function2
+#!/usr/bin/env python
+from code.app import fetch_wrap_file
+import sys
 
 def main():
-    result1 = function1()
-    result2 = function2()
-    print("Result from function1:", result1)
-    print("Result from function2:", result2)
+    if len(sys.argv) < 2:
+        print("Usage: fossil-wrap.py <package_name_1> <package_name_2> ...")
+        sys.exit(1)
+
+    package_names = sys.argv[1:]
+    
+    for package_name in package_names:
+        fetch_wrap_file(package_name)
 
 if __name__ == "__main__":
     main()
